89ee8162e99735693d8d07f53909455a4c4348aa
Seunggak Park <parksg1992@gmail.com>
2022-02-20 14:46:01 -0600

    Price Checker

--- Main.Designer.cs
+++ Main.Designer.cs
@@ -31,6 +31,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -63,11 +64,22 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(25, 271);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(112, 46);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(292, 244);
+            this.ClientSize = new System.Drawing.Size(292, 339);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -82,5 +94,6 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
--- Main.cs
+++ Main.cs
@@ -9,6 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace HardLiquor_Sales
 {
@@ -35,12 +36,12 @@ namespace HardLiquor_Sales
         {
             string test1 = "Test1";
             string test2 = "Test2";
-            
+
             using (StreamWriter wr = new StreamWriter("test.csv"))
             {
                 wr.WriteLine("Article ID,Quantity");
                 wr.WriteLine("{0},{1}", test1, test2);
-                
+
                 MessageBox.Show("File Create Test Complete", "Message Box");
             }
 
@@ -54,6 +55,110 @@ namespace HardLiquor_Sales
         {
             LocalSavings localSaving = new LocalSavings();
             localSaving.ShowDialog();
+        }
+
+
+        /// //////////////////////////////////////////////////////////////////////
+
+        Excel.Application application;
+        Excel.Workbook workbook;
+        Excel.Worksheet worksheet;
+        object[,] itemNumRawData;
+        object[,] itemUPCRawData;
+        object[,] itemDescRawData;
+        object[,] itemPkRawData;
+        object[,] itemTgpSrpRawData;
+        object[,] itemLandedCostRawData;
+
+        OpenFileDialog OrderOFD = new OpenFileDialog();
+        public static string TestFilePath = "";
+        int orderFileCnt = 0;
+        ItemInfo_Test[] itemInfo_Test;
+
+        public struct ItemInfo_Test
+        {
+            public string itemNumber;
+            public string itemUPC;
+            public string itemDesc;
+            public string itemPk;
+            public string itemTgpSrp;
+            public string itemLandedCost;
+
+        }
+
+        public string SetFilePath(bool isOrderFile)
+        {
+            if (isOrderFile == true)
+            {
+                return OrderOFD.FileName;
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (OrderOFD.ShowDialog() == DialogResult.OK)
+            {
+                TestFilePath = SetFilePath(true);
+            }
+            string destinationFile = TestFilePath;
+
+            application = new Excel.Application();
+            workbook = application.Workbooks.Open(Filename: destinationFile);
+            worksheet = workbook.Worksheets.get_Item(1);
+            application.Visible = false;
+
+            orderFileCnt = worksheet.UsedRange.Rows.Count;
+
+            // Item Number
+            Excel.Range itemNumStartRange = worksheet.Cells[4, 6];
+            Excel.Range itemNumEndRange = worksheet.Cells[orderFileCnt, 6];
+            Excel.Range itemNumRange = worksheet.get_Range(itemNumStartRange, itemNumEndRange);
+            itemNumRawData = itemNumRange.Value;
+
+            // Item UPC
+            Excel.Range itemUPCStartRange = worksheet.Cells[4, 7];
+            Excel.Range itemUPCEndRange = worksheet.Cells[orderFileCnt, 7];
+            Excel.Range itemUPCRange = worksheet.get_Range(itemUPCStartRange, itemUPCEndRange);
+            itemUPCRawData = itemUPCRange.Value;
+
+            // Item Desc
+            Excel.Range itemDescStartRange = worksheet.Cells[4, 8];
+            Excel.Range itemDescEndRange = worksheet.Cells[orderFileCnt, 8];
+            Excel.Range itemDescRange = worksheet.get_Range(itemDescStartRange, itemDescEndRange);
+            itemDescRawData = itemDescRange.Value;
+
+            // Item PK
+            Excel.Range itemPkStartRange = worksheet.Cells[4, 9];
+            Excel.Range itemPkEndRange = worksheet.Cells[orderFileCnt, 9];
+            Excel.Range itemPkRange = worksheet.get_Range(itemPkStartRange, itemPkEndRange);
+            itemPkRawData = itemPkRange.Value;
+
+            // Item TGP SRP
+            Excel.Range itemTgpSrpStartRange = worksheet.Cells[4, 14];
+            Excel.Range itemTgpSrpEndRange = worksheet.Cells[orderFileCnt, 14];
+            Excel.Range itemTgpSrpRange = worksheet.get_Range(itemTgpSrpStartRange, itemTgpSrpEndRange);
+            itemTgpSrpRawData = itemTgpSrpRange.Value;
+
+            // Item Landed Cost
+            Excel.Range itemLandedCostStartRange = worksheet.Cells[4, 16];
+            Excel.Range itemLandedCostEndRange = worksheet.Cells[orderFileCnt, 16];
+            Excel.Range itemLandedCostRange = worksheet.get_Range(itemLandedCostStartRange, itemLandedCostEndRange);
+            itemLandedCostRawData = itemLandedCostRange.Value;
+
+
+            string savePath = @"C:\Users\rhehf\Downloads\test.xml";
+
+            for (int i = 1; i <= itemNumRawData.GetLength(0); i++)
+            {
+                string itemInfoText = "<itemInfo item_num= \"" + itemNumRawData[i, 1].ToString() + "\"" + " upc = \"" + itemUPCRawData[i, 1].ToString() + "\"" +
+                    " desc = \"" + itemDescRawData[i,1].ToString() + "\"" + " pk = \"" + itemPkRawData[i,1].ToString() + "\"" + " TGP_srp = \""+ itemTgpSrpRawData[i,1].ToString() +
+                   "\"" + " Landed_cost = \"" + itemLandedCostRawData[i,1].ToString() + "\"" + "/>" + "\r\n";
+                System.IO.File.AppendAllText(savePath, itemInfoText, Encoding.Default);
+            }
         }
     }
 }
